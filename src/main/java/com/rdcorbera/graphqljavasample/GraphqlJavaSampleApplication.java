84084ffb1fd757d5a3b799f1acdc286ace50c4ed
Ricardo Corbera <rdcorbera@gmail.com>
2021-04-19 00:45:59 -0400

    Initial project

---
+++ src/main/java/com/rdcorbera/graphqljavasample/GraphqlJavaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.rdcorbera.graphqljavasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJavaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlJavaSampleApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/rdcorbera/graphqljavasample/GraphqlJavaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdcorbera.graphqljavasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlJavaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
